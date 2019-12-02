1611592609cc121c7b9fe0976c900246eada24bc
yiyiyiyai <373811088@qq.com>
2019-12-02 16:48:54 +0800

    first commit(配置&创建所需的包)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'lib-flexible'
+
+if (module.hot) {
+    module.hot.accept();
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
